4546eabcc5abcc9e2212301249490ddde50ce148
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-10-25 22:01:22 +0600

    applicatoin variable to keep the visitor count

--- CRUDwithMVC_EF/Global.asax.cs
+++ CRUDwithMVC_EF/Global.asax.cs
@@ -12,10 +12,28 @@ namespace CRUDwithMVC_EF
     {
         protected void Application_Start()
         {
+            Application["Totaluser"] = 0;
+            /* Application["Totaluser"] = 0;
+             * This variable will be declared once the application is started and will keep the value till the application stopped. 
+             * So once the application launched, it will be counting on the visitors until the application is online*/
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Session_Start()
+        {
+            Application.Lock();
+            Application["Totaluser"] = (int)Application["Totaluser"] + 1;
+            Application.UnLock();
+        }
+        protected void Session_End()
+        {
+            Application.Lock();
+            Application["Totaluser"] = (int)Application["Totaluser"] - 1;
+            Application.UnLock();
+        }
+
     }
 }
