1f1258cfc825703f6b00b12545c64d116cf0f241
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-10-25 17:55:21 +0600

    Migrations for Department Entity adding

---
+++ CRUDwithMVC_EF/Migrations/202010241545380_DepartmentEntityAdded.cs
@@ -0,0 +1,33 @@
+﻿namespace CRUDwithMVC_EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DepartmentEntityAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Department",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Employee", "Department_Id", c => c.Int());
+            CreateIndex("dbo.Employee", "Department_Id");
+            AddForeignKey("dbo.Employee", "Department_Id", "dbo.Department", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employee", "Department_Id", "dbo.Department");
+            DropIndex("dbo.Employee", new[] { "Department_Id" });
+            DropColumn("dbo.Employee", "Department_Id");
+            DropTable("dbo.Department");
+        }
+    }
+}
